53b2584d7b3830d69021f7230323bda1369ae584
Seio924 <seio9241@gmail.com>
2024-03-24 15:22:42 +0900

    글 수정

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 let db;
 const url =
@@ -25,26 +25,15 @@ new MongoClient(url)
     console.log(err);
   });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", async (req, res) => {
+  let result = await db.collection("post").find().toArray();
+  res.render("list.ejs", { posts: result });
 });
 
 app.get("/write", (req, res) => {
   //DB에 저장하기
   //db.collection("post").insertOne({ title: "어쩌구" });
   res.render("write.ejs");
-});
-
-app.get("/list", async (req, res) => {
-  let result = await db.collection("post").find().toArray();
-
-  res.render("list.ejs", { posts: result });
-});
-
-app.get("/time", (req, res) => {
-  let time = new Date();
-
-  res.render("time.ejs", { servertime: time });
 });
 
 app.post("/add", async (req, res) => {
@@ -61,4 +50,27 @@ app.post("/add", async (req, res) => {
     console.log(e);
     res.status(500).send("서버 에러");
   }
+});
+
+app.get("/detail/:id", async (req, res) => {
+  try {
+    let result = await db
+      .collection("post")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    if (result == null) {
+      res.status(400).send("이상한 url 입력");
+    } else {
+      res.render("detail.ejs", { posts: result });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).send("이상한 url 입력");
+  }
+});
+
+app.get("/edit:id", async (req, res) => {
+  let result = await db.collection.findOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.render("edit.ejs", { posts: result });
 });
